e7009b36635b3faa79913505fb7d11bf13b7546c
LeandraOliveiraS <you@example.com>
2020-04-14 16:57:23 -0300

    Verificador de validade de formas normais conjuntivas

--- cnf.py
+++ cnf.py
@@ -1,28 +1,26 @@
 #coding: utf-8
 
-quantidade = int(input())
-qtd_validas = 0
-clausulas = []
-atomos = []
-resultado = []
-
-def temLiteraisComplementares(lista):
-    for i in range(len(lista)-1):
-        for j in range(len(lista)):
-            if lista[i] == ("!" + lista[j]):
+def ehValida(lista):
+    for i in range(len(lista)):
+        if lista[0] == "!":
+             if (lista[i] in lista):
                 return True
-            elif("!" + lista[i]) == lista[j]:
+        else:
+            if ("!"  +  lista [i]) in lista:
                 return True
-    return False
             
-for i in range(quantidade):
+    return False
+
+qtd_formulas = int(input())
+clausulas = []
+atomos = set()
+
+for i in range(qtd_formulas):
     clausulas = input().lower().split("&")
     for j in range (len(clausulas)):
         atomos = clausulas[j].replace("(","").replace(")","").split("|")
-    if temLiteraisComplementares(atomos) == True:
-        resultado.append("eh valido")
+    if ehValida(atomos) == True:
+        print("eh valida")
     else:
-        resultado.append("não eh válido")
+        print("não eh válida")
 
-for result in range(len(resultado)):
-    print(resultado[result])
