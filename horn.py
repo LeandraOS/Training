63a523b9562055cf8f375650e797337216dcb571
LeandraOliveiraS <you@example.com>
2020-05-03 04:07:26 -0300

    Verificacao de satisfatibilidade concluida

--- horn.py
+++ horn.py
@@ -3,10 +3,8 @@
 atomos_separados = {}
 
 qtd_formulas = int(input())
-contador_de_formulas = 0
 separada = list()
 false = "F"
-
 
 for i in range(qtd_formulas):
     formulas = input().replace(' ', '').lower().split("&")
@@ -15,5 +13,4 @@ for i in range(qtd_formulas):
     if (false.lower() not in separada):
         print ("{}.satisfazível".format(i + 1))
     else:
-        
-print(separada)
+        #verificacao de quando a formula nao eh satisfazivel
